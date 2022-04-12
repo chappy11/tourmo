52f1f412c99410c0aa57288009146ff9474f4a2a
Jeffrey <jeffrey.mata@mlhuillier.com>
2022-04-12 15:48:01 +0800

    search bar

--- src/screen/vehicle/AddVehicle.js
+++ src/screen/vehicle/AddVehicle.js
@@ -1,12 +1,14 @@
-import { Picker } from '@react-native-picker/picker';
+
 import React from 'react'
 import {View,StyleSheet,ImageBackground,Text,ScrollView,TouchableOpacity,FlatList} from 'react-native'
 import ImagePicker from 'react-native-image-crop-picker';
-import { Button, Caption, Title } from 'react-native-paper';
+import { Button as Rbutton, Caption, Title } from 'react-native-paper';
 import { TextInput } from 'react-native-paper';
 import RNscreen from '../../components/RNscreen';
 import Screen from '../../components/Screen';
 import API from '../../endpoints/API';
+import { Button } from '../../components/Button';
+import { Color } from '../../utils/Themes';
 const AddVehicle = ({ navigation, route }) => {
     const [data, setdata] = React.useState({
         pic1: "",
@@ -24,7 +26,29 @@ const AddVehicle = ({ navigation, route }) => {
         setdata({...data,[name]:value})
     }
 
-    const frontImage = () => {
+    const getmotorcycle = (nam,bran,trans) => {
+        setdata({
+            ...data,
+            name: nam,
+            brand: bran,
+            transmission:trans
+        })
+        setisSelect(false)
+        
+    }
+
+
+
+    const frontImage = (type) => {
+        if (type == "picker") {
+            ImagePicker.openPicker({
+                width: 300,
+                height: 400,
+                compressImageQuality:0.7
+            }).then(res => {
+                onChange("pic1")
+            })
+        }
         ImagePicker.openCamera({
             width: 300,
             height: 400,
@@ -74,51 +98,64 @@ const AddVehicle = ({ navigation, route }) => {
         })
     }
 
-    console.log(route.params.m_id)
+  console.log("Inputs",data)
     return (
         <RNscreen style={{backgroundColor:'white'}}>
             {isSelect ?
                 (
                     <>
-                    <Choose/>
+                        <Choose getmotorcycle={getmotorcycle} setisSelect={setisSelect} />
                     </>
                 )
                 :
                     (
-                <ScrollView style = {{ flex: 1 }}>
+                <ScrollView style={{ flex: 1 }}>
+                <View style={{ flexDirection:'row',padding:15,justifyContent:'center',alignItems:'center'}}>
+                    <Text style={{flex:1,fontSize:18,fontWeight:'bold'}}>Motorcycle :</Text>
+                    <Rbutton onPress={()=>setisSelect(true)}>SEARCH</Rbutton>    
+                </View>
+                <View style={style.nameContainer}>
+                    <Caption>Brand</Caption>          
+                    <Text style={{marginLeft:10,fontSize:18}}>{ data.brand}</Text>                
+                </View>
+                <View style={style.nameContainer}>
+                    <Caption>Name</Caption>          
+                    <Text style={{marginLeft:10,fontSize:18}}>{ data.name}</Text>                
+                </View>
+                <View style={style.nameContainer}>
+                    <Caption>Transmission</Caption>          
+                    <Text style={{marginLeft:10,fontSize:18}}>{ data.transmission}</Text>                
+                </View>        
                 <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={frontImage}>
                             <ImageBackground source={{uri:data.pic1}} style={style.imagebackground}>
                                     <Text>Front View</Text>
                             </ImageBackground>
                         </TouchableOpacity>
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={sideImage}>
                             <ImageBackground style={style.imagebackground} source={{uri:data.pic2}}>
                                     <Text>Side View</Text>
                             </ImageBackground>
                         </TouchableOpacity>
                  </View>
                 <View style={{justifyContent:'center',alignItems:'center'}}>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={backImage}>
                         <ImageBackground style={style.imagebackground} source={{ uri: data.pic3 }}>
                             <Text>Back View</Text>
                         </ImageBackground>
                     </TouchableOpacity>
                 </View>
-                <View style={{ flexDirection:'row'}}>
-                    <Text>Motorcycle :</Text>
-                    <Button onPress={()=>setisSelect(true)}>SEARCH</Button>    
-                </View>
+            
                 <View style={{flexDirection:'row',justifyContent:'center'}}>
-                    <TouchableOpacity>
-                        <ImageBackground style={style.imagebackground}>
+                    <TouchableOpacity onPress={orImage}>
+                                <ImageBackground style={style.imagebackground} source={{uri:data.or}}>
                             <Text>Official Reciept Photo</Text>
                         </ImageBackground>
                     </TouchableOpacity>
                 </View>
                   <View style={{flexDirection:'row',justifyContent:'center'}}>
-                    <TouchableOpacity>
-                            <ImageBackground style={style.imagebackground}>
+                    <TouchableOpacity onPress={crImage}>
+                            <ImageBackground style={style.imagebackground} source={{uri:data.cr}}>
                                 <Text>Certifition of Registration</Text>
                             </ImageBackground>
                     </TouchableOpacity>
@@ -155,14 +192,17 @@ const style = StyleSheet.create({
         borderTopWidth: 1.5,
         borderBottomWidth: 1.5,
         borderColor: 'whitesmoke',
-        padding:10
-        
-    }
+        padding: 10
+    },
+    nameContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 5
+    },
 })
 
 
 
-export const Choose = () => {
+export const Choose = ({getmotorcycle,setisSelect}) => {
     const [list, setlist] = React.useState([]);
     const [backup, setbackup] = React.useState([]);
     const [isEmpty, setisEmpty] = React.useState(false);
@@ -202,11 +242,13 @@ export const Choose = () => {
     
 
     const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={()=>getmotorcycle(item.name,item.brand,item.transmission)}>
         <View style={style.renderItem}>
             <Title>{item.name}</Title>
             <Text>{item.brand}</Text>
             <Text>{item.transmission}</Text>
         </View>
+    </TouchableOpacity>
     );
     return (
         <View style={{flex:1}}>
@@ -222,6 +264,7 @@ export const Choose = () => {
                 renderItem={renderItem}
                 style={{ flex:1}}
             />
+            <Button name="Close" color={Color.danger} mode='contained' style={{ color: 'white' }} onPress={()=>setisSelect(false)}/> 
         </View>
     )
 }
