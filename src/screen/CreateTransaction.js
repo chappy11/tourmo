8a2153981b84b1d0dfc737f91f1881b3f355910b
chappy11 <76852841+chappy11@users.noreply.github.com>
2022-04-26 09:06:22 +0800

    Add files via upload

--- src/screen/CreateTransaction.js
+++ src/screen/CreateTransaction.js
@@ -1,7 +1,7 @@
-import { View, Text, StyleSheet,TouchableOpacity,Image } from 'react-native'
+import { View, Text, StyleSheet,TouchableOpacity,Image,Alert } from 'react-native'
 import React,{useState} from 'react'
 import RNscreen from '../components/RNscreen'
-import { Caption, Dialog, Headline, Modal, Portal } from 'react-native-paper'
+import { Caption, Dialog, Headline, Modal, Portal,} from 'react-native-paper'
 import { Button } from '../components/Button'
 import { TextInput } from '../components/TextInput'
 import { Color } from '../utils/Themes'
@@ -11,22 +11,38 @@ import { Func } from '../utils/Func'
 import { hour,  min } from '../components/Time'
 import { Picker } from '@react-native-picker/picker'
 import {UserContext} from '../context/Context';
+import API from '../endpoints/API';
+
 
 const CreateTransaction = ({route}) => {
-    const {user} = React.useContext(UserContext);
-    const [state, setstate] = useState({
-         hr: "hour",
-         min: "minute",
-         type:null, 
-         start: "YYYY-MM-DD",
-         end: "YYYY-MM-DD",
-    })
+  const { user } = React.useContext(UserContext);
+  const [isLoad, setisLoad] = React.useState(false);
+  const params = route.params.data;
+  const [total, settotal] = React.useState(0);
+ const [state, setstate] = useState({
+         hr: "01",
+        min: "00",
+        isAm:"AM", 
+        type:null, 
+        start: "YYYY-MM-DD",
+        end: "YYYY-MM-DD",
+        no_days:"" 
+  })
   const listhours = hour();
   const [type, settype] = useState("");
   const [showCalendar, setshowCalendar] = React.useState(false);
     
   const [isView, setisView] = useState(false);
   
+  React.useCallback(() => {
+    if (state.end !== "YYYY-MM-DD") {
+      onChange("no_days", Func.daterange(state.start, state.end));
+      settotal((Func.daterange(state.start, state.end)) * params.rate);
+    } 
+   
+   
+  },[state.end])
+
   const openCalendar = (type) => {
     settype(type)
     setshowCalendar(true)
@@ -35,8 +51,39 @@ const CreateTransaction = ({route}) => {
     const onChange = (name,val) => {
       setstate({...state,[name]:val})
   }
- 
-  console.log("USER",user)
+  
+  console.log(state.no_days)
+
+  const submit = () => {
+   
+    setisLoad(true);
+    if (state.hr === "" || state.min === "") {
+      Alert.alert("Warning", "Please specific time");
+      setisLoad(false)
+    }
+    else if (state.start === "YYYY-MM-DD" || state.end === "YYYY-MM-DD") {
+      Alert.alert("Warning", "Please Date start and End");
+         setisLoad(false)
+    } else {
+      let payload = {
+        motor_id: params.motor_id,
+        user_id: user.user_id,
+        time: state.hr + ":" + state.min + " " + state.isAm,
+        date_start: state.start,
+        date_end:state.end
+      }
+      API.createBookings(payload).then(res => {
+        if (res.data.status === 1) {
+          Alert.alert("Success", res.data.message);
+          
+        } else {
+          Alert.alert("Error", res.data.message);
+        }
+         setisLoad(false)
+      })
+    }
+  }
+  //console.log("USER",user)
 
   
 
@@ -80,44 +127,48 @@ const CreateTransaction = ({route}) => {
                         </View>
                     </View>
             </View>
-            <View style={{flexDirection:'row',width:'100%'}}>
-                <View style={{flex:1,flexDirection:'row'}}>
-                                <Picker
-                    style={{ flex: 1 }}
+                <View>
+                  <Caption>Time Start</Caption>
+                </View>    
+                <View style={style.border}>
+                  <Picker
                     selectedValue={state.hr}
-                    onValueChange={(e)=>onChange("hr",e)}
-                      >
-                        {hour().map(val => (
-                          <Picker.Item label={val} value={val}/>
-                        ))}     
-                    </Picker>
-                            <View style={{justifyContent:'center'}}>
-                                 <Text>Hour</Text>
-                    </View>
-                   
+                    onValueChange={(val)=>onChange("hr",val)}
+                    
+                  >
+                    {hour().map(val => (
+                      <Picker.Item label={val} value={val}/>
+                      ))}
+                  </Picker>
+                </View>
+                 <View style={style.border}>
+                  <Picker
+                    selectedValue={state.min}
+                    onValueChange={(val)=>onChange("min",val)}
+                    
+                  >
+                    {min().map(val => (
+                      <Picker.Item label={val} value={val}/>
+                      ))}
+                  </Picker>
+                </View>
+                <View style={style.border}>
+                  <Picker
+                    selectedValue={state.isAm}
+                    onValueChange={(val)=>onChange("isAm",val)}
+                  >
+                    <Picker.Item label='AM' value="AM" />
+                      <Picker.Item label='PM' value="PM"/>
+                  </Picker>
+                </View>
+               <View>
+               
+                  <Button disabled={isLoad ? true :false} name="Create Booking" color={Color.primary} mode='contained' onPress={submit}/>
                 </View>
                 
-                  <View style={{ justifyContent:'center',paddingHorizontal:10}}>
-                    <Text>:</Text>
-                  </View>
-                  <View style={{flexDirection:'row',flex:1}}>
-                      <Picker
-                          style={{ flex: 1 }}
-                          selectedValue={state.min}
-                         onValueChange={(e)=>setstate({...state,min:e})}
-                           >
-                          {min().map(val => (
-                              <Picker.Item label={val} value={val}/>
-                            ))}
-                        </Picker>
-                         <View style={{justifyContent:'center'}}>
-                                 <Text>minute</Text>
-                    </View>
-                  </View>
-                
-            </View>
-          </View>               
           </View>
+             
+     </View>
         
           )
        
@@ -135,5 +186,11 @@ const style = StyleSheet.create({
         marginHorizontal: 10,
         marginVertical: 5,
         padding:15
-    }
+  },
+  border: {
+    padding: 5,
+    borderWidth: 1,
+    borderColor: 'whitesmoke',
+    marginVertical:10
+  }
 })
