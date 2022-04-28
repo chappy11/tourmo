11ce11533b394c0270c6f09d8b91af3dd1dd5797
chappy11 <76852841+chappy11@users.noreply.github.com>
2022-04-28 17:28:26 +0800

    Updated function

--- src/utils/Func.js
+++ src/utils/Func.js
@@ -2,27 +2,34 @@ import React from 'react';
 import moment from 'moment';
 export const Func = {
     isEmail: (email) => {
-        let valid =  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/
+        let valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/
         return valid.test(email);
     },
-    isValidPhone: (phone) =>{
+    isValidPhone: (phone) => {
         let valid = /^(09|\+639) \d{ 9 } $/;
         return valid.test(valid);
     },
-    filename:(file) =>{
+    filename: (file) => {
         return file.split(/(\\|\/)/g).pop();
     },
     dateformat: (x) => {
-        let date = new Date(x); 
-         const year = date.getFullYear();
-         const month = (date.getMonth() +1).toString().padStart(2,0);
-         const day = date.getDate().toString().padStart(2,0);
-         return year + "-" + month + "-" + day;
+        let date = new Date(x);
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, 0);
+        const day = date.getDate().toString().padStart(2, 0);
+        return year + "-" + month + "-" + day;
     },
-    daterange:(date1,date2)=>{
+    daterange: (date1, date2) => {
         var start = moment(date1);
         var end = moment(date2);
-        return end.diff(start, "days")
-        
+        return end.diff(start, "days");
+    },
+    datebetween:(startDate, endDate) => {
+       let date = []
+        while(moment(startDate) <= moment(endDate)){
+         date.push(startDate);
+        startDate = moment(startDate).add(1, 'days').format("YYYY-MM-DD");
+        }
+            return date;
     }
 }
