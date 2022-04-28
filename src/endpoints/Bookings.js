a7c0563ace89f2fad52091f2c93444c34159db97
chappy11 <76852841+chappy11@users.noreply.github.com>
2022-04-28 17:20:56 +0800

    Adding Accept get date

--- src/endpoints/Bookings.js
+++ src/endpoints/Bookings.js
@@ -12,5 +12,17 @@ export default  {
     gelistofbookings:async (id) =>{
         let resp = await Axios.get(ip+"booking/getbookinglist/"+id);
         return resp.data;
+    },
+    getbookingbymotor: async (id) => {
+        let resp = await Axios.get(ip+"booking/getbymotorid/"+id)
+        return resp.data;
+    },
+    getdatelist: async (id) => {
+        let resp = await Axios.get(ip + "booking/getdatelist/" + id);
+        return resp.data;
+    },
+    acceptbooking: async (booking_id) =>{
+        let resp = await Axios.patch(ip + "booking/accept/" + id);
+        return resp.data;
     }
 }
